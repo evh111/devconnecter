09d6aea660cbd013fb0bb87ebdb4f06224bbac1d
Elijah Helmandollar <evh111@elijahs-air.attlocal.net>
2019-05-14 16:56:36 -0400

    Added partial front end

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,15 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-sm'>Logout</span>
@@ -20,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='#!'>Developers</Link>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
